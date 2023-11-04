65c6306087a014385ff1cd1c13353bd334635750
RaminMajidi <raminmajididev01@gmail.com>
2023-11-04 22:53:30 +0330

    change welcom text

--- client/src/admin/dashbord/components/main/Main.jsx
+++ client/src/admin/dashbord/components/main/Main.jsx
@@ -1,9 +1,12 @@
 import './Main.css'
 import Dashbord from '../../Dashbord'
+import { useContext } from 'react'
+import { AdminContext } from '../../../context/context'
 const Main = () => {
+    const { userData } = useContext(AdminContext)
     return (
-        <Dashbord>
-            <h1 className='is-size-3'>سلام ، به پنل مدیریت خوش آمدید</h1>
+        <Dashbord title="داشبورد">
+            <h1 className='is-size-3'>سلام <span className='has-text-success'>{userData?.name}</span> ، به پنل مدیریت خوش آمدید</h1>
             <h3 className='is-size-5 mt-3'>امیدوارم خبرهای خوبی داشته باشی</h3>
         </Dashbord>
     )
